462655133040029285099d8b6fb47f48d5c9a444
omenta <omenta@users.noreply.github.com>
2022-03-13 22:45:46 -0500

    Update ticketing.py
    Under 65 tix price is $10. 65 and over tix price is $8. Groups of 5 or more get a 20% discount on their ticket prices.

--- ticketing.py
+++ ticketing.py
@@ -1,1 +1,28 @@
-
+def movie_tix():
+	price = 1
+	movie_lovers = {}
+	persons = int(input('Party: '))
+	for i in range(persons):
+		name = input('Name: ')
+		age = int(input('Age: '))
+		movie_lovers[name] = age
+	
+	print(movie_lovers) # TEMP
+	print('NAME-------PRICE')
+	for i in movie_lovers:
+		if movie_lovers[i] > 0 and movie_lovers[i] < 65:
+			if persons >= 5:
+				price = 10 * 0.8
+			else:
+				price = 10
+		elif movie_lovers[i] >= 65:
+			if persons >= 5:
+				price = 8 * 0.8
+			else:
+				price = 8
+		else:
+			# Invalid age
+			print('Error!')
+		print(i, '     $', price)
+			
+movie_tix()
